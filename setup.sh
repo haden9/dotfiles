5f77d3abcce123d3944af5e11a7edd1589381f14
haden9 <haden9@gmail.com>
2016-03-05 17:04:40 -0600

    Update setup.sh message

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Simply bash to setup the .vimrc file
 full_pwd="$(pwd -P)"
-echo "Creating .vimrc HOME dir symlink..."
+echo "Creating .vimrc and .tmux.conf HOME dir symlink..."
 ln -s -f $full_pwd/.vimrc ~/.vimrc
 ln -s -f $full_pwd/.tmux.conf ~/.tmux.conf
 echo "Done!"
